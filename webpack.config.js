316b1881c643b010834738fb4beb52b70f23855e
nickj12497 <njohnson@adelyte.com>
2015-07-06 20:26:23 -0400

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+webpack = require('webpack')
+
+module.exports = {
+    entry: "src/todo",
+    output: {
+        path: './dist',
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.jade$/, loader: 'jade' },
+            { test: /\.coffee$/, loader: 'coffee' },
+            { test: /\.json$/, loader: 'html?name=assets/data/[name].[ext]' },
+            { test: /\.sass$/, loader: 'sass' }
+        ]
+    }
+};
